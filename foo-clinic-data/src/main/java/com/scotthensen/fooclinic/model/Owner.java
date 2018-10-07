9a9b72c530ce4f3ba6640ab029a19217211e4718
ScottHensen <hensen.scott@gmail.com>
2018-10-06 22:06:58 -0700

    lombok builder

--- foo-clinic-data/src/main/java/com/scotthensen/fooclinic/model/Owner.java
+++ foo-clinic-data/src/main/java/com/scotthensen/fooclinic/model/Owner.java
@@ -9,18 +9,33 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table( name = "owners" )
 public class Owner extends Person
 {
+	@Builder
+	public Owner(Long id, String firstName, String lastName, 
+				 String address, String city, String telephone, Set<Pet> pets) 
+	{
+		super(id, firstName, lastName);
+		this.address = address;
+		this.city = city;
+		this.telephone = telephone;
+		this.pets = pets;
+	}
+	
 	@Column( name = "address" )
 	private String address;
 	
+
 	@Column( name = "city" )
 	private String city;
 	
